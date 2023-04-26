2dbae7e35733527a270e7d29b0b6cc2954b1a4ba
junesdream <norisori@gmail.com>
2023-04-26 16:47:36 +0200

    Fixed IT for get&post Methode

---
+++ backend/src/main/java/de/rainbowdev/backend/model/RavgroovUser.java
@@ -0,0 +1,18 @@
+package de.rainbowdev.backend.model;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+public record RavgroovUser(
+
+        String id,
+        String userImg,
+        String email,
+        List<String> friends,
+        Timestamp joiningDate
+
+) {
+    public RavgroovUser() {
+        this(null, null, null, null, null);
+    }
+}
--- backend/src/test/java/de/rainbowdev/backend/controller/PostIntegrationTest.java
+++ backend/src/test/java/de/rainbowdev/backend/controller/PostIntegrationTest.java
@@ -7,15 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 import org.springframework.http.MediaType;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class PostIntegrationTest {
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+class PostIntegrationTest {
 
     @Autowired
     MockMvc mockMvc;
@@ -32,6 +35,7 @@ public class PostIntegrationTest {
                         ]
                         """));
     }
+
     @Test
     void addPost() throws Exception {
         mockMvc.perform(post("/api/posts")
