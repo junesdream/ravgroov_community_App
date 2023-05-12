16778e67a2ec1a291e3ec3bb1488bfe85251ab52
junesdream <norisori@gmail.com>
2023-05-12 12:55:18 +0200

    Resolved the duplications in RightBar.tsx

--- frontend/src/components/rightbar/RightBar.tsx
+++ frontend/src/components/rightbar/RightBar.tsx
@@ -41,11 +41,11 @@ export default function RightBar() {
                     <div className="right_user">
                         <div className="right_userInfo">
                             <img
-                                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                                src="https://images.pexels.com/photos/15245100/pexels-photo-15245100.jpeg?auto=compress&cs=tinysrgb&w=1200"
                                 alt=""
                             />
                             <p>
-                                <span>Jane Doe</span> changed their cover picture
+                                <span>Sammy Kerny</span> changed their cover picture
                             </p>
                         </div>
                         <span>1 min ago</span>
@@ -53,35 +53,35 @@ export default function RightBar() {
                     <div className="right_user">
                         <div className="right_userInfo">
                             <img
-                                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                                src="https://images.pexels.com/photos/16553641/pexels-photo-16553641.jpeg?auto=compress&cs=tinysrgb&w=1200"
                                 alt=""
                             />
                             <p>
-                                <span>Jane Doe</span> changed their cover picture
+                                <span>Emmy Paington</span> changed their cover Music
                             </p>
                         </div>
-                        <span>1 min ago</span>
+                        <span>2 min ago</span>
                     </div>
                     <div className="right_user">
                         <div className="right_userInfo">
                             <img
-                                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                                src="https://images.pexels.com/photos/8218386/pexels-photo-8218386.jpeg?auto=compress&cs=tinysrgb&w=1200"
                                 alt=""
                             />
                             <p>
-                                <span>Jane Doe</span> changed their cover picture
+                                <span>Flower Solar</span> changed their cover picture
                             </p>
                         </div>
-                        <span>1 min ago</span>
+                        <span>3 min ago</span>
                     </div>
                     <div className="right_user">
                         <div className="right_userInfo">
                             <img
-                                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                                src="https://images.pexels.com/photos/1659438/pexels-photo-1659438.jpeg?auto=compress&cs=tinysrgb&w=1200"
                                 alt=""
                             />
                             <p>
-                                <span>Jane Doe</span> changed their cover picture
+                                <span>High Mountain</span> changed their cover picture
                             </p>
                         </div>
                         <span>1 min ago</span>
@@ -92,111 +92,111 @@ export default function RightBar() {
                     <div className="right_user">
                         <div className="right_userInfo">
                             <img
-                                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                                src="https://images.pexels.com/photos/16542270/pexels-photo-16542270.jpeg?auto=compress&cs=tinysrgb&w=1200"
                                 alt=""
                             />
                             <div className="right_online" />
-                            <span>Jane Doe</span>
+                            <span>Rommy Oslon</span>
                         </div>
                     </div>
                     <div className="right_user">
                         <div className="right_userInfo">
                             <img
-                                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                                src="https://images.pexels.com/photos/8347501/pexels-photo-8347501.jpeg?auto=compress&cs=tinysrgb&w=1200"
                                 alt=""
                             />
                             <div className="right_online" />
-                            <span>Jane Doe</span>
+                            <span>Zolla Dolla</span>
                         </div>
                     </div>
                     <div className="right_user">
                         <div className="right_userInfo">
                             <img
-                                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                                src="https://images.pexels.com/photos/16656616/pexels-photo-16656616.jpeg?auto=compress&cs=tinysrgb&w=1200"
                                 alt=""
                             />
                             <div className="right_online" />
-                            <span>Jane Doe</span>
+                            <span>Jam Jammy</span>
                         </div>
                     </div>
                     <div className="right_user">
                         <div className="right_userInfo">
                             <img
-                                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                                src="https://images.pexels.com/photos/2340166/pexels-photo-2340166.jpeg?auto=compress&cs=tinysrgb&w=1200"
                                 alt=""
                             />
                             <div className="right_online" />
-                            <span>Jane Doe</span>
+                            <span>Strong Music</span>
                         </div>
                     </div>
                     <div className="right_user">
                         <div className="right_userInfo">
                             <img
-                                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                                src="https://images.pexels.com/photos/5342002/pexels-photo-5342002.png?auto=compress&cs=tinysrgb&w=1200"
                                 alt=""
                             />
                             <div className="right_online" />
-                            <span>Jane Doe</span>
+                            <span>Watch Dotch</span>
                         </div>
                     </div>
                     <div className="right_user">
                         <div className="right_userInfo">
                             <img
-                                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                                src="https://images.pexels.com/photos/11595991/pexels-photo-11595991.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
                                 alt=""
                             />
                             <div className="right_online" />
-                            <span>Jane Doe</span>
+                            <span>Noir Lady</span>
                         </div>
                     </div>
                     <div className="right_user">
                         <div className="right_userInfo">
                             <img
-                                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                                src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&w=1200"
                                 alt=""
                             />
                             <div className="right_online" />
-                            <span>Jane Doe</span>
+                            <span>Juddy Wunndy</span>
                         </div>
                     </div>
                     <div className="right_user">
                         <div className="right_userInfo">
                             <img
-                                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                                src="https://images.pexels.com/photos/609549/pexels-photo-609549.jpeg?auto=compress&cs=tinysrgb&w=1200"
                                 alt=""
                             />
                             <div className="right_online" />
-                            <span>Jane Doe</span>
+                            <span>Eays Esra</span>
                         </div>
                     </div>
                     <div className="right_user">
                         <div className="right_userInfo">
                             <img
-                                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                                src="https://images.pexels.com/photos/3618162/pexels-photo-3618162.jpeg?auto=compress&cs=tinysrgb&w=1200"
                                 alt=""
                             />
                             <div className="right_online" />
-                            <span>Jane Doe</span>
+                            <span>Killiman Tree</span>
                         </div>
                     </div>
                     <div className="right_user">
                         <div className="right_userInfo">
                             <img
-                                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                                src="https://images.pexels.com/photos/1624076/pexels-photo-1624076.jpeg?auto=compress&cs=tinysrgb&w=1200"
                                 alt=""
                             />
                             <div className="right_online" />
-                            <span>Jane Doe</span>
+                            <span>Happy Flower</span>
                         </div>
                     </div>
                     <div className="right_user">
                         <div className="right_userInfo">
                             <img
-                                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                                src="https://images.pexels.com/photos/2690323/pexels-photo-2690323.jpeg?auto=compress&cs=tinysrgb&w=1200"
                                 alt=""
                             />
                             <div className="right_online" />
-                            <span>Jane Doe</span>
+                            <span>Brilla Mong</span>
                         </div>
                     </div>
                 </div>
