d0e6e01a9bf134091ee9f52d5188a02fe016d3ed
junesdream <norisori@gmail.com>
2023-05-19 15:42:33 +0200

    Added addpost button on postgallery

--- frontend/src/components/PostGallery/PostGallery.tsx
+++ frontend/src/components/PostGallery/PostGallery.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {Post} from "../../model/Post";
 import './PostGallery.css';
 import PostCard from "../PostCard/PostCard";
+import {Link} from "react-router-dom";
 
 
 type Props = {
@@ -13,6 +14,9 @@ export default function PostGallery(props: Props) {
     return (
         <div className='post_gallery'>
             <h1>Posts Gallery </h1>
+            <Link className="post_gallery_Btn" to="/posts/add">
+                <button >Add Post</button>
+            </Link>
             {
                 props.posts.map((post) => <PostCard key={post.id} post={post} deletePost={props.deletePost}/>)
             }
