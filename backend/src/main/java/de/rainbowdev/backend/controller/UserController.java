5a72d28e2259bd59aff11ef03ccf805fb1a5ea0d
junesdream <norisori@gmail.com>
2023-05-20 17:18:36 +0200

    Added signuo controller & Tests

--- backend/src/main/java/de/rainbowdev/backend/controller/UserController.java
+++ backend/src/main/java/de/rainbowdev/backend/controller/UserController.java
@@ -1,12 +1,23 @@
 package de.rainbowdev.backend.controller;
 
+import de.rainbowdev.backend.model.MongoUser;
+import de.rainbowdev.backend.service.UserService;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/me")
     public String getMe() {
@@ -18,6 +29,20 @@ public class UserController {
     public String login() {
         return SecurityContextHolder.getContext().getAuthentication().getName();
     }
+
+
+    @PostMapping("/signup")
+    public ResponseEntity<MongoUser> createUser(@RequestBody MongoUser mongoUser) {
+        MongoUser createdUser = userService.createMongoUser(mongoUser);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    }
+
+
+    @GetMapping("/{username}")
+    public MongoUser loadMongoUserByName(@PathVariable String username) {
+        return userService.findUserByUsername(username);
+    }
+
 
     @PostMapping("/logout")
     public void logout(HttpSession httpSession) {
---
+++ backend/src/main/java/de/rainbowdev/backend/service/UserService.java
@@ -0,0 +1,35 @@
+package de.rainbowdev.backend.service;
+
+import de.rainbowdev.backend.model.MongoUser;
+import de.rainbowdev.backend.repository.MongoUserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class UserService {
+    private final MongoUserRepository mongoUserRepository;
+    private final PasswordEncoder encoder;
+
+    public UserService(MongoUserRepository mongoUserRepository, PasswordEncoder encoder) {
+        this.mongoUserRepository = mongoUserRepository;
+        this.encoder = encoder;
+    }
+
+    public MongoUser createMongoUser(MongoUser mongoUser) {
+
+        if (mongoUserRepository.findMongoUserByUsername(mongoUser.username()).isPresent()) {
+            throw new IllegalArgumentException("The username already exists.");
+        } else {
+            String encodedPassword = encoder.encode(mongoUser.password());
+            MongoUser encodedUser = new MongoUser(mongoUser.id(), mongoUser.username(), encodedPassword);
+            return mongoUserRepository.save(encodedUser);
+        }
+    }
+
+    public MongoUser findUserByUsername(String username) {
+        return mongoUserRepository.findMongoUserByUsername(username).orElseThrow(NoSuchElementException::new);
+    }
+
+}
