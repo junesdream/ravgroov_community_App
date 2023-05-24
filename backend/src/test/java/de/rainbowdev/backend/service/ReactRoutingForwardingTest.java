3105c7229a2f83364aaf097d91d44d29f2ed909c
junesdream <norisori@gmail.com>
2023-05-24 12:11:34 +0200

    Fixed ReactRoutingForwardingTest

--- backend/src/test/java/de/rainbowdev/backend/service/ReactRoutingForwardingTest.java
+++ backend/src/test/java/de/rainbowdev/backend/service/ReactRoutingForwardingTest.java
@@ -7,13 +7,14 @@ import org.springframework.core.io.Resource;
 
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 class ReactRoutingForwardingTest {
 
     @Test
     void testExistingResource() throws IOException {
+        // Arrange
         String resourcePath = "/static/image.jpg";
         Resource mockLocation = mock(Resource.class);
         Resource mockResource = mock(Resource.class);
@@ -22,12 +23,17 @@ class ReactRoutingForwardingTest {
         when(mockLocation.createRelative(resourcePath)).thenReturn(mockResource);
 
         ReactRoutingForwarding.ReactRoutingPathResourceResolver resolver = new ReactRoutingForwarding.ReactRoutingPathResourceResolver();
+
+        // Act
         Resource returnedResource = resolver.getResource(resourcePath, mockLocation);
+
+        // Assert
         assertEquals(mockResource, returnedResource);
     }
 
     @Test
     void testNonExistingResource() throws IOException {
+        // Arrange
         String resourcePath = "/foo/bar";
         Resource mockLocation = mock(Resource.class);
         Resource mockResource = mock(Resource.class);
@@ -35,12 +41,17 @@ class ReactRoutingForwardingTest {
         when(mockLocation.createRelative(resourcePath)).thenReturn(mockResource);
 
         ReactRoutingForwarding.ReactRoutingPathResourceResolver resolver = new ReactRoutingForwarding.ReactRoutingPathResourceResolver();
+
+        // Act
         Resource returnedResource = resolver.getResource(resourcePath, mockLocation);
+
+        // Assert
         assertEquals(new ClassPathResource(ReactRoutingForwarding.DEFAULT_STARTING_PAGE), returnedResource);
     }
 
     @Test
     void testUnreadableResource() throws IOException {
+        // Arrange
         String resourcePath = "/static/image.jpg";
         Resource mockLocation = mock(Resource.class);
         Resource mockResource = mock(Resource.class);
@@ -49,11 +60,26 @@ class ReactRoutingForwardingTest {
         when(mockLocation.createRelative(resourcePath)).thenReturn(mockResource);
 
         ReactRoutingForwarding.ReactRoutingPathResourceResolver resolver = new ReactRoutingForwarding.ReactRoutingPathResourceResolver();
+
+        // Act
         Resource returnedResource = resolver.getResource(resourcePath, mockLocation);
+
+        // Assert
         assertEquals(new ClassPathResource(ReactRoutingForwarding.DEFAULT_STARTING_PAGE), returnedResource);
     }
 
+    @Test
+    void testIOExceptionOnCreateRelative() throws IOException {
+        // Arrange
+        String resourcePath = "/static/image.jpg";
+        Resource mockLocation = mock(Resource.class);
+        when(mockLocation.createRelative(resourcePath)).thenThrow(IOException.class);
+
+        ReactRoutingForwarding.ReactRoutingPathResourceResolver resolver = new ReactRoutingForwarding.ReactRoutingPathResourceResolver();
+
+        // Act & Assert
+        assertThrows(IOException.class, () -> {
+            resolver.getResource(resourcePath, mockLocation);
+        });
+    }
 }
-
-
-
